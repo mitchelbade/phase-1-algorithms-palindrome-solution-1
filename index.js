13a77072a03dcf09d3e88306c5d738aac3789d4d
mitchelbade <mitchelm.bade@gmail.com>
2022-02-03 14:52:48 -0800

    commit tree f9af7aaf4db6530041583b7030e81809986583d4

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverseString(word) {
+  const wordLower = word.toLowerCase();
+  const wordArray = wordLower.split("");
+  const wordReverseArray = wordArray.reverse();
+  const wordReverse = wordReverseArray.join("");
+  return wordReverse;
+  //return word.toLowerCase().split("").reverse().join("");
+  
 }
+
+function isPalindrome(word) {
+  const reversedWord = reverseString(word);
+  return word === reversedWord;
+};
 
 /* 
   Add your pseudocode here
+  write a function that takes in a string
+  reverses the word
+  if the reverse is the same as original word
+  return true
+  else
+  return false
 */
 
 /*
   Add written explanation of your solution here
+  makes a function that checks if a word is a palindrome
+  palindrome = word is the same forwards and backwards example - mom, racecar, dad, madam
 */
 
 // You can run `node index.js` to view these console logs
